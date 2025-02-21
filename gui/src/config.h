9da0ca7b554697d87a27847ec77f13764284795c
ifilot <ivo@ivofilot.nl>
2025-02-21 10:57:12 +0100

    Small adjustment to ROMs collection

--- gui/src/config.h
+++ gui/src/config.h
@@ -22,7 +22,7 @@
 #define _CONFIG_H
 
 #define PROGRAM_NAME "PICO SST39sf0x0 Programmer"
-#define PROGRAM_VERSION "1.7.1"
+#define PROGRAM_VERSION "1.7.2"
 #define UNUSED(x) (void)(x)
 
 #endif // _CONFIG_H
